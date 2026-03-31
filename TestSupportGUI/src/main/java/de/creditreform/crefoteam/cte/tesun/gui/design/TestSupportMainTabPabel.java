517d13ccd682b5776876399e0693175b5e8dfcaf
Kemal cavdar <kemal@cavdar.de>
2026-03-31 17:42:06 +0200

    GUI für ACTIVITI-Explorer

--- TestSupportGUI/src/main/java/de/creditreform/crefoteam/cte/tesun/gui/design/TestSupportMainTabPabel.java
+++ TestSupportGUI/src/main/java/de/creditreform/crefoteam/cte/tesun/gui/design/TestSupportMainTabPabel.java
@@ -41,6 +41,10 @@ public class TestSupportMainTabPabel extends JTabbedPane {
         return viewTestResults;
     }
 
+    public JButton getButtonACTITIExporer() {
+        return buttonACTITIExporer;
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         panelLogs = new JPanel();
@@ -51,56 +55,58 @@ public class TestSupportMainTabPabel extends JTabbedPane {
         scrollPanelProcessImage = new JScrollPane();
         labelProcessImage = new JLabel();
         viewTestResults = new TestResultsView();
+        buttonACTITIExporer = new JButton();
 
         //======== this ========
 
         //======== panelLogs ========
         {
-           panelLogs.setLayout(new GridBagLayout());
-           ((GridBagLayout)panelLogs.getLayout()).columnWidths = new int[] {0, 0, 0};
-           ((GridBagLayout)panelLogs.getLayout()).rowHeights = new int[] {0, 0, 0};
-           ((GridBagLayout)panelLogs.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0E-4};
-           ((GridBagLayout)panelLogs.getLayout()).rowWeights = new double[] {1.0, 0.0, 1.0E-4};
+            panelLogs.setLayout(new GridBagLayout());
+            ((GridBagLayout)panelLogs.getLayout()).columnWidths = new int[] {0, 0, 0};
+            ((GridBagLayout)panelLogs.getLayout()).rowHeights = new int[] {0, 0, 0};
+            ((GridBagLayout)panelLogs.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0E-4};
+            ((GridBagLayout)panelLogs.getLayout()).rowWeights = new double[] {1.0, 0.0, 1.0E-4};
 
-           //======== scrollPaneTaskListenerInfo ========
-           {
+            //======== scrollPaneTaskListenerInfo ========
+            {
 
-              //---- textAreaTaskListenerInfo ----
-              textAreaTaskListenerInfo.setTabSize(4);
-              textAreaTaskListenerInfo.setLineWrap(true);
-              textAreaTaskListenerInfo.setEditable(false);
-              textAreaTaskListenerInfo.setBackground(Color.white);
-              textAreaTaskListenerInfo.setFont(new Font("Verdana", Font.PLAIN, 12));
-              textAreaTaskListenerInfo.setMinimumSize(new Dimension(455, 16));
-              scrollPaneTaskListenerInfo.setViewportView(textAreaTaskListenerInfo);
-           }
-           panelLogs.add(scrollPaneTaskListenerInfo, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0,
-              GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-              new Insets(0, 0, 0, 0), 0, 0));
+                //---- textAreaTaskListenerInfo ----
+                textAreaTaskListenerInfo.setTabSize(4);
+                textAreaTaskListenerInfo.setLineWrap(true);
+                textAreaTaskListenerInfo.setEditable(false);
+                textAreaTaskListenerInfo.setBackground(Color.white);
+                textAreaTaskListenerInfo.setFont(new Font("Verdana", Font.PLAIN, 12));
+                textAreaTaskListenerInfo.setMinimumSize(new Dimension(455, 16));
+                scrollPaneTaskListenerInfo.setViewportView(textAreaTaskListenerInfo);
+            }
+            panelLogs.add(scrollPaneTaskListenerInfo, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
 
-           //---- checkBoxScrollToEnd ----
-           checkBoxScrollToEnd.setText("Auto-Scroll");
-           panelLogs.add(checkBoxScrollToEnd, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
-              GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-              new Insets(2, 2, 2, 2), 0, 0));
+            //---- checkBoxScrollToEnd ----
+            checkBoxScrollToEnd.setText("Auto-Scroll");
+            panelLogs.add(checkBoxScrollToEnd, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(2, 2, 2, 2), 0, 0));
 
-           //---- buttonClearLOGPanel ----
-           buttonClearLOGPanel.setIcon(new ImageIcon(getClass().getResource("/icons/table_replace.png")));
-           buttonClearLOGPanel.setActionCommand("LOG's leeren");
-           buttonClearLOGPanel.setToolTipText("");
-           buttonClearLOGPanel.setText("LOG-Panel leeren");
-           panelLogs.add(buttonClearLOGPanel, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
-              GridBagConstraints.EAST, GridBagConstraints.VERTICAL,
-              new Insets(2, 2, 2, 2), 0, 0));
+            //---- buttonClearLOGPanel ----
+            buttonClearLOGPanel.setIcon(new ImageIcon(getClass().getResource("/icons/table_replace.png")));
+            buttonClearLOGPanel.setActionCommand("clearLOGs");
+            buttonClearLOGPanel.setToolTipText("LOG's aus dem Panel l\u00f6schen");
+            buttonClearLOGPanel.setText("LOG's l\u00f6schen");
+            panelLogs.add(buttonClearLOGPanel, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+                GridBagConstraints.EAST, GridBagConstraints.VERTICAL,
+                new Insets(2, 2, 2, 2), 0, 0));
         }
-        addTab("Logs", panelLogs);
+        addTab("LOG's", panelLogs);
 
         //======== scrollPanelProcessImage ========
         {
-           scrollPanelProcessImage.setViewportView(labelProcessImage);
+            scrollPanelProcessImage.setViewportView(labelProcessImage);
         }
-        addTab("Prozess-Image", scrollPanelProcessImage);
+        addTab("Prozess", scrollPanelProcessImage);
         addTab("Test-Results", viewTestResults);
+        addTab("ACTITI-Exporer", new ImageIcon(getClass().getResource("/icons/xhtml.png")), buttonACTITIExporer);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
@@ -113,5 +119,6 @@ public class TestSupportMainTabPabel extends JTabbedPane {
     private JScrollPane scrollPanelProcessImage;
     private JLabel labelProcessImage;
     private TestResultsView viewTestResults;
+    private JButton buttonACTITIExporer;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
